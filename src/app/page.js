aadf2e64ffaf5d579823b186badd4cd653ad998d
Yousif <yousifae8@gmail.com>
2026-02-14 18:27:31 +0200

    Refactor styles and components: - Cleaned up CSS files by removing commented-out code and unused styles in add.module.css, dashboard.module.css, expenses.module.css, login.module.css, and signup.module.css. - Updated page.js files for add, dashboard, expenses, and signup components to remove unused code and improve readability. - Removed favicon.ico and page.js, page.module.css files as they are no longer needed. - Streamlined supabase.js by uncommenting and organizing the code for better clarity.

--- src/app/page.js
+++
@@ -1,9 +0,0 @@
-
-// import { Signup } from "./signup/page";
-export default function Home() {
-  return (
-    <div>
-      
-    </div>
-  );
-}
--- src/app/supabase.js
+++ src/app/supabase.js
@@ -1,15 +1,3 @@
-// import { createClient } from "@supabase/supabase-js";
-// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-
-
-// const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
-// export default supabase
-
-//Remove this commented-out block to clean up the file.
-
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
